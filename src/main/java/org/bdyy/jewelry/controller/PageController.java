b903c128344cde90e71124cc7d7f2cd080feb99b
zhengkai <1374685349@qq.com>
2018-09-19 15:54:41 +0800

    update4

--- src/main/java/org/bdyy/jewelry/controller/PageController.java
+++ src/main/java/org/bdyy/jewelry/controller/PageController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
-public class UserController {
+public class PageController {
 
     @Resource
     private UserService userService;
